a25188508b6e960597d5b6039a60d6079a6b743d
Gokul <gokulchandan24@gmail.com>
2024-01-18 23:42:27 +0530

    app creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { animation: 'isRight' },
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { animation: 'isRight' },
+  },
+  {
+    path: 'movie/:type/:id',
+    component: MovieComponent,
+    data: { animation: 'isRight' },
+  },
+  { path: '**', component: LoginComponent },
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, HttpClientModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  trendingMovies: any;
+  theatreMovies: any;
+  popularMovies: any;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getTrendingMovies();
+    this.getTheatreMovies();
+    this.getPopularMovies();
+  }
+
+  getTrendingMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/trending-movies.json')
+      .subscribe((movies) => {
+        this.trendingMovies = movies;
+      });
+  }
+
+  getTheatreMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/theatre-movies.json')
+      .subscribe((movies) => {
+        this.theatreMovies = movies;
+      });
+  }
+
+  getPopularMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/popular-movies.json')
+      .subscribe((movies) => {
+        this.popularMovies = movies;
+      });
+  }
+
+  goToMovie(type: string, id: string) {
+    this.router.navigate(['movie', type, id]);
+  }
+}
